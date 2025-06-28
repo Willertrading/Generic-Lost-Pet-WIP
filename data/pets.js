b20f86cc9834caa72e19914f35943c75b51e0381
Willertrading <willertrading@gmail.com>
2025-06-27 20:30:54 -0400

    Add files via upload

---
+++ data/pets.js
@@ -0,0 +1,64 @@
+window.PET_DATA = {
+  shadow: {
+    name: "Shadow",
+    species: "Cat",
+    breed: "American Short Hair",
+    color: "Black Hair",
+    sex: "Male",
+    birthday: "2023-07-28",
+    img: "images/Shadow.jpg",
+    about: `My name is Shadow.\nI am an outdoor cat.\nI have had all my shots and have been neutered.\nI am a sweet boy that loves catnip, tuna and long naps!`,
+    owners: `Dan and Shannon Willer\n20780 Coleman Brake Road\nMilford Center, Ohio 43045`,
+    contacts: `Vet: Community Veterinary Clinic - 614-612-0326\nDan Willer: 614-657-2849\nShannon Willer: 614-216-7671`,
+    ownerPhone: "+16142167671",
+    
+  },
+
+  // Add more pets like this:
+  bella: {
+    name: "Momma",
+    species: "Cat",
+    breed: "American Shorthair",
+    color: "Tuxedo",
+    sex: "Female",
+    birthday: "2020-03-12",
+    img: "images/momma.JPG",
+    about: `Hi, I'm Bella!\nI love people and I'm very friendly.\nI love Dan.`,
+    owners: `John and Jane Doe\n123 Main St\nSpringfield, Ohio 43000`,
+    contacts: `Vet: Sprin Animal Hospital - 614-555-0199\nJohn: 614-555-1234\nJane: 614-555-5678`,
+    ownerPhone: "+16146572849",
+    
+  },
+    
+  loki: {
+    name: "Loki",
+    species: "Dog",
+    breed: "Pug",
+    color: "Tan",
+    sex: "Male",
+    birthday: "2015-03-12",
+    img: "images/momma.JPG",
+    about: `Hi, I'm Loki!\nI love people and I'm very friendly.\nI have no allergies.\nI love beer.`,
+    owners: `CJ Knapp and Scott Garrett\n14285 NW 65th Place\nParkville, MO 64152`,
+    contacts: `Vet: Parkville Heights Animal Hospital - 816-741-2247\nCJ: 816-719-8259\nScott: 816-719-7599`,
+    ownerPhone: "+18167197599",
+
+   },
+    
+  banner: {
+    name: "Banner",
+    species: "Dog",
+    breed: "Pug",
+    color: "Tan",
+    sex: "Male",
+    birthday: "2010-03-12",
+    img: "images/momma.JPG",
+    about: `Hi, I'm Banner!\nI love marking my territory.\nI have no allergies.\nI love snuggling.`,
+    owners: `CJ Knapp and Scott Garrett\n14285 NW 65th Place\nParkville, MO 64152`,
+    contacts: `Vet: Parkville Heights Animal Hospital - 816-741-2247\nCJ: 816-719-8259\nScott: 816-719-7599`,
+    ownerPhone: "+18167197599", 
+  }
+
+  
+};
+
